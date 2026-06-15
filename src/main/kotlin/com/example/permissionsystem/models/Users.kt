70fbbacb2f027af05dacc15a5bae142cb28399a8
Mazen <mazen@mzn.dev>
2026-06-15 16:13:53 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/permissionsystem/models/Users.kt
@@ -0,0 +1,3 @@
+package com.example.permissionsystem.models
+
+data class User( val useId: Int, val assignedRole: Long)
---
+++ src/main/kotlin/com/example/permissionsystem/service/Operations.kt
@@ -0,0 +1,5 @@
+package com.example.permissionsystem.service
+
+import com.example.permissionsystem.models.User
+
+val users = mutableMapOf<Int, User>()
