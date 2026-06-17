6ad28b2a4f59c1de49a934b82071829cafcd9bba
Mazen <mazen@mzn.dev>
2026-06-17 14:30:59 +0300

    Implemented addRole operation in Operations.kt

--- src/main/kotlin/com/example/permissionsystem/service/Operations.kt
+++ src/main/kotlin/com/example/permissionsystem/service/Operations.kt
@@ -14,10 +14,25 @@ class Operations {
 
         val matchedUser = users[userId] ?: throw IllegalArgumentException("User with id $userId not found")
 
-        val matchedRole = UserRole.entries.find { it.name == roleName.uppercase() }
+        val matchedRole = UserRole.entries.find {
+            it.name == roleName.uppercase()
+        }
         matchedRole ?: throw IllegalArgumentException("Role $roleName not found")
 
         val rolesList = UserRole.entries.filter { (it.rolesMask and matchedUser.assignedRole) != 0L }
 
+        if (matchedRole in rolesList) {
+            val stringRolesList : List<String> = rolesList.map { it.name }
+            return RoleOperationResult("User with id $userId already has the role ${matchedRole.name}", stringRolesList)
+        } else {
+            val newMask = matchedUser.assignedRole or matchedRole.rolesMask
+            val finalRolesList = UserRole.entries.filter { (it.rolesMask and newMask) != 0L }
+            val stringFinalRolesList : List<String> = finalRolesList.map { it.name }
+
+            val newUserRoles = matchedUser.copy(assignedRole = newMask)
+            users[userId] = newUserRoles
+
+            return RoleOperationResult("Role $matchedRole was added to the user with $userId successfully", stringFinalRolesList)
+        }
     }
 }
