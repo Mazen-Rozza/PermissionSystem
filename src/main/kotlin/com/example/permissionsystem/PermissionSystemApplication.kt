b174889b08ea6543ca6851fde474624dc563d4dc
Mazen <mazen@mzn.dev>
2026-06-18 12:28:38 +0300

    implemented deleteRole operation and added test print statement for it

--- src/main/kotlin/com/example/permissionsystem/PermissionSystemApplication.kt
+++ src/main/kotlin/com/example/permissionsystem/PermissionSystemApplication.kt
@@ -1,5 +1,6 @@
 package com.example.permissionsystem
 
+import com.example.permissionsystem.dto.RoleOperationResult
 import com.example.permissionsystem.service.Operations
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -16,6 +17,8 @@ class PermissionSystemApplication
 fun main() {
     val operationInstance = Operations()
 
+    println("addRole operation test")
+
     try {
         operationInstance.addRole(7, "admin")
     } catch (e: IllegalArgumentException) {
@@ -28,15 +31,31 @@ fun main() {
         println("Error occurred : ${e.message}")
     }
 
+    val duplicatedRole = operationInstance.addRole(2, "adMin")
+    println("${duplicatedRole.msg}, Roles: ${duplicatedRole.roles}")
+
+    val addRoleResult = operationInstance.addRole(3, "guest")
+    println("${addRoleResult.msg}, Roles: ${addRoleResult.roles}")
+
+    println("\n")
+
+    println("deleteRole operation test")
+
     try {
-        operationInstance.addRole(2, "adMin")
+        operationInstance.deleteRole(7, "admin")
     } catch (e: IllegalArgumentException) {
         println("Error occurred : ${e.message}")
     }
 
     try {
-        operationInstance.addRole(3, "guest")
+        operationInstance.deleteRole(3, "pizza")
     } catch (e: IllegalArgumentException) {
         println("Error occurred : ${e.message}")
     }
+
+    val deleteRoleOperation = operationInstance.deleteRole(4, "guest")
+    println("${deleteRoleOperation.msg}, Roles: ${deleteRoleOperation.roles}")
+
+    val userNoRole = operationInstance.deleteRole(3, "support")
+    println("${userNoRole.msg}, Roles: ${userNoRole.roles}")
 }
--- src/main/kotlin/com/example/permissionsystem/service/Operations.kt
+++ src/main/kotlin/com/example/permissionsystem/service/Operations.kt
@@ -9,36 +9,70 @@ import org.springframework.stereotype.Service
 class Operations {
 
     val users = mutableMapOf(
-    1 to User(1, 0L),
-    2 to User(2, UserRole.ADMIN.rolesMask),
-    3 to User(3, UserRole.ADMIN.rolesMask or UserRole.MODERATOR.rolesMask),
-    4 to User(4, UserRole.GUEST.rolesMask or UserRole.SUPPORT.rolesMask or UserRole.MANAGER.rolesMask),
-    5 to User(5, UserRole.SUPER_ADMIN.rolesMask)
+        1 to User(1, 0L),
+        2 to User(2, UserRole.ADMIN.rolesMask),
+        3 to User(3, UserRole.ADMIN.rolesMask or UserRole.MODERATOR.rolesMask),
+        4 to User(4, UserRole.GUEST.rolesMask or UserRole.SUPPORT.rolesMask or UserRole.MANAGER.rolesMask),
+        5 to User(5, UserRole.SUPER_ADMIN.rolesMask)
     )
 
     fun addRole(userId: Int, roleName: String): RoleOperationResult {
-
-        val matchedUser = users[userId] ?: throw IllegalArgumentException("User with id $userId not found")
-
-        val matchedRole = UserRole.entries.find {
-            it.name == roleName.uppercase()
-        }
-        matchedRole ?: throw IllegalArgumentException("Role $roleName not found")
-
+        val matchedUser = validateUserExists(userId)
+        val matchedRole = validateRoleExists(roleName)
         val rolesList = UserRole.entries.filter { (it.rolesMask and matchedUser.assignedRole) != 0L }
 
         if (matchedRole in rolesList) {
-            val stringRolesList : List<String> = rolesList.map { it.name }
+            val stringRolesList: List<String> = rolesList.map { it.name }
             return RoleOperationResult("User with id $userId already has the role ${matchedRole.name}", stringRolesList)
         } else {
             val newMask = matchedUser.assignedRole or matchedRole.rolesMask
             val finalRolesList = UserRole.entries.filter { (it.rolesMask and newMask) != 0L }
-            val stringFinalRolesList : List<String> = finalRolesList.map { it.name }
-
+            val stringFinalRolesList: List<String> = finalRolesList.map { it.name }
             val newUserRoles = matchedUser.copy(assignedRole = newMask)
-            users[userId] = newUserRoles
 
-            return RoleOperationResult("Role $matchedRole was added to the user with $userId successfully", stringFinalRolesList)
+            users[userId] = newUserRoles
+            return RoleOperationResult(
+                "Role $matchedRole was added to the user with id $userId successfully",
+                stringFinalRolesList
+            )
         }
+    }
+
+    fun deleteRole(userId: Int, roleName: String): RoleOperationResult {
+        val matchedUser = validateUserExists(userId)
+        val matchedRole = validateRoleExists(roleName)
+        val rolesList = UserRole.entries.filter { (it.rolesMask and matchedUser.assignedRole) != 0L }
+
+        if (matchedRole in rolesList) {
+            val deleteMask = matchedRole.rolesMask.inv()
+            val newMask = deleteMask and matchedUser.assignedRole
+            val finalRolesList = UserRole.entries.filter { (it.rolesMask and newMask) != 0L }
+            val stringFinalRolesList: List<String> = finalRolesList.map { it.name }
+            val newUserRoles = matchedUser.copy(assignedRole = newMask)
+
+            users[userId] = newUserRoles
+            return RoleOperationResult(
+                "Role $matchedRole was deleted successfully from the user with id $userId",
+                stringFinalRolesList
+            )
+        } else {
+            val stringRolesList: List<String> = rolesList.map { it.name }
+            return RoleOperationResult(
+                "User with id $userId doesn't have the role ${matchedRole.name}",
+                stringRolesList
+            )
+        }
+    }
+
+    fun validateUserExists(userId: Int): User {
+        return users[userId] ?: throw IllegalArgumentException("User with id $userId not found")
+    }
+
+    fun validateRoleExists(roleName: String): UserRole {
+        val matchedRole = UserRole.entries.find {
+            it.name == roleName.uppercase()
+        }
+        matchedRole ?: throw IllegalArgumentException("Role $roleName not found")
+        return matchedRole
     }
 }
